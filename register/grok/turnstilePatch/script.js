616cecd0b13ae925d71153f404a79e7834785ac8
ThinkerWen <296854007@qq.com>
2026-03-27 15:35:35 +0800

    fix(workspace): 漏提验证码环境伪装补丁

---
+++ register/grok/turnstilePatch/script.js
@@ -0,0 +1,37 @@
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let screenX = getRandomInt(800, 1200);
+let screenY = getRandomInt(400, 600);
+
+Object.defineProperty(MouseEvent.prototype, 'screenX', { value: screenX });
+Object.defineProperty(MouseEvent.prototype, 'screenY', { value: screenY });
+
+try {
+    Object.defineProperty(navigator, 'webdriver', {
+        get: () => undefined,
+        configurable: true,
+    });
+} catch (e) {}
+
+try {
+    Object.defineProperty(navigator, 'languages', {
+        get: () => ['zh-CN', 'zh', 'en-US', 'en'],
+        configurable: true,
+    });
+} catch (e) {}
+
+try {
+    Object.defineProperty(navigator, 'platform', {
+        get: () => 'MacIntel',
+        configurable: true,
+    });
+} catch (e) {}
+
+try {
+    Object.defineProperty(navigator, 'plugins', {
+        get: () => [1, 2, 3, 4, 5],
+        configurable: true,
+    });
+} catch (e) {}
